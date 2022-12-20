91fa33aefcf6e33f8b058c85533f6f32fd8a2a90
Andy.R <avroberts@outlook.com>
2022-12-20 17:36:47 +0000

    removed middle queue layer prior to prices insert

--- purple/utils/http_reader.py
+++ purple/utils/http_reader.py
@@ -1,13 +1,15 @@
 import logging
 import requests
+import csv
 import os
 
-#---------------------------------------------------------------------------------------#   
+# ---------------------------------------------------------------------------------------#
+
+
 def stream_file():
     """create a generator to stream-read HTTP file lines (records). 
         https://2.python-requests.org/en/master/user/advanced/#id9
         Args:
-            path: the path to the data.
             Return: a generator of file records
     """
     url = os.getenv('PriceDataURL')
@@ -22,4 +24,110 @@ def stream_file():
     except requests.RequestException as ex:
         logging.error('Unable to load data from HTTP: {}'.format(ex))
         raise ex
-    
+
+# ---------------------------------------------------------------------------------------#
+
+
+def stream_file_for_outcode(outcode):
+    """create a generator to stream-read HTTP file lines (records). 
+        https://2.python-requests.org/en/master/user/advanced/#id9
+        Args:
+            outcode: the outcode of records to return.
+            Return: a generator of file records
+    """
+    url = os.getenv('PriceDataURL')
+    logging.info(f'Streaming price file from {url}.')
+    try:
+        with requests.get(url, stream=True) as f:
+            for line in f.iter_lines():
+                if line:
+                    csv_rec = format_price_rec(line.decode('utf-8'))
+                    # ignore records without a populated postcode
+                    if csv_rec is not None:
+                        # filter just the outcode records we need.
+                        if csv_rec['Postcode'].split(' ')[0].lower() == outcode:
+                            yield csv_rec
+
+    except requests.RequestException as ex:
+        logging.error('Unable to load data from HTTP: {}'.format(ex))
+        raise ex
+
+# ---------------------------------------------------------------------------------------#
+
+
+def format_address(paon, saon, street):
+    """join the parts of the record that form the unique address component.
+        Args:   
+            paon, saon, street: strings representing components of address line 1
+            Return: the address string
+    """
+    address_seq = []
+    if paon:
+        address_seq.append(paon)
+    if saon:
+        address_seq.append(saon)
+    if street:
+        address_seq.append(street)
+
+    address_string = ' '.join(address_seq)
+    return address_string
+
+# ---------------------------------------------------------------------------------------#
+
+
+def format_price_rec(rec):
+    """format the csv record for storage in a queue message. 
+        Args:   
+            rec: a CSV string representing a data record of columns
+            Return: a dictionary of the record to be added to the queue
+    """
+    in_cols = ['RowKey', 'Price', 'PriceDate', 'Postcode', 'PropertyType',
+               'NewBuild', 'Duration', 'Paon', 'Saon', 'Street', 'Locality',
+               'Town', 'District', 'County', 'PpdCategory', 'RecStatus']
+
+    decoded_rec = csv.DictReader([rec], in_cols)
+    price_rec = {}
+
+    for cols in decoded_rec:
+        if len(cols['Postcode']) > 0:
+            price_rec['Postcode'] = cols['Postcode']
+            price_rec['Address'] = format_address(
+                cols['Paon'], cols['Saon'], cols['Street'])
+            price_rec['Price'] = int(cols['Price'])
+            price_rec['Date'] = cols['PriceDate'][0:10]  # yyyy-mm-dd only
+            price_rec['Locality'] = cols['Locality']
+            price_rec['Town'] = cols['Town']
+            price_rec['District'] = cols['District']
+            price_rec['County'] = cols['County']
+            price_rec['RecStatus'] = cols['RecStatus']
+
+        else:
+            price_rec = None
+
+    return price_rec
+
+# ---------------------------------------------------------------------------------------#
+
+
+def format_price_config(rec):
+    """for CSV files, decode a single line that may be quote delimited with embedded 
+       commas. Return the record as a dict with only the data you want.
+        Args:   
+            rec: a CSV string representing a data record of columns
+            Return: the dict of needed column names and values
+    """
+    in_cols = ['RowKey', 'Price', 'PriceDate', 'Postcode', 'PropertyType',
+               'NewBuild', 'Duration', 'Paon', 'Saon', 'Street', 'Locality',
+               'Town', 'District', 'County', 'PpdCategory', 'RecStatus']
+
+    out_cols = ['Price', 'Postcode']
+
+    decoded_rec = csv.DictReader([rec], in_cols)
+    # shallow copy the next (actually, the only one) dictreader entry
+    in_dict = next(decoded_rec).copy()
+    # use filter() function to remove unwanted columns
+    # (note, t[0] identifies the keys from in_dict but you could use t[1] for the values)
+    out_dict = {k: v for k, v in filter(
+        lambda t: t[0] in out_cols, in_dict.items())}
+
+    return out_dict
--- purple/utils/landreg_decoder.py
+++ purple/utils/landreg_decoder.py
@@ -1,6 +1,8 @@
 import csv
 
-#---------------------------------------------------------------------------------------#
+# ---------------------------------------------------------------------------------------#
+
+
 def prune_rec_for_queue(rec):
     """for CSV files, decode a single line that may be quote delimited with embedded 
        commas. Return the record as a dict with only the data you want.
@@ -8,24 +10,26 @@ def prune_rec_for_queue(rec):
             rec: a CSV string representing a data record of columns
             Return: the dict of needed column names and values
     """
-    in_cols = ['RowKey','Price','PriceDate','Postcode','PropertyType',\
-               'NewBuild','Duration','Paon','Saon','Street','Locality',\
-               'Town','District','County','PpdCategory','RecStatus']
-    
-    out_cols = ['Price','PriceDate','Postcode',\
-                'Paon','Saon','Street','Locality',\
-                'Town','District','County']
+    in_cols = ['RowKey', 'Price', 'PriceDate', 'Postcode', 'PropertyType',
+               'NewBuild', 'Duration', 'Paon', 'Saon', 'Street', 'Locality',
+               'Town', 'District', 'County', 'PpdCategory', 'RecStatus']
+
+    out_cols = ['Price', 'PriceDate', 'Postcode',
+                'Paon', 'Saon', 'Street', 'Locality',
+                'Town', 'District', 'County']
 
     decoded_rec = csv.DictReader([rec], in_cols)
     # shallow copy the next (actually, the only one) dictreader entry
     in_dict = next(decoded_rec).copy()
-    # use filter() function to remove unwanted columns 
+    # use filter() function to remove unwanted columns
     # (note, t[0] identifies the keys from in_dict but you could use t[1] for the values)
-    out_dict = {k : v for k,v in filter(lambda t: t[0] in out_cols, in_dict.items())}
+    out_dict = {k: v for k, v in filter(
+        lambda t: t[0] in out_cols, in_dict.items())}
     return out_dict
 
+# ---------------------------------------------------------------------------------------#
 
-#---------------------------------------------------------------------------------------#
+
 def format_address(paon, saon, street, postcode):
     """join the parts of the record that form the unique address component.
         Args:   
@@ -39,31 +43,33 @@ def format_address(paon, saon, street, postcode):
         address_seq.append(saon)
     if street:
         address_seq.append(street)
-    #if postcode:
+    # if postcode:
     #    address_seq.append(postcode)
 
     address_string = ' '.join(address_seq)
     return address_string
 
+# ---------------------------------------------------------------------------------------#
 
-#---------------------------------------------------------------------------------------#
+
 def format_for_msg(rec):
     """format the csv record for storage in a queue message. 
         Args:   
             rec: a CSV string representing a data record of columns
             Return: a dictionary of the record to be added to the queue
     """
-    in_cols = ['RowKey','Price','PriceDate','Postcode','PropertyType',\
-               'NewBuild','Duration','Paon','Saon','Street','Locality',\
-               'Town','District','County','PpdCategory','RecStatus']
-    
+    in_cols = ['RowKey', 'Price', 'PriceDate', 'Postcode', 'PropertyType',
+               'NewBuild', 'Duration', 'Paon', 'Saon', 'Street', 'Locality',
+               'Town', 'District', 'County', 'PpdCategory', 'RecStatus']
+
     decoded_rec = csv.DictReader([rec], in_cols)
     price_rec = {}
 
     for cols in decoded_rec:
         if len(cols['Postcode']) > 0:
             price_rec['Postcode'] = cols['Postcode']
-            price_rec['Address'] = format_address(cols['Paon'], cols['Saon'], cols['Street'], cols['Postcode'])
+            price_rec['Address'] = format_address(
+                cols['Paon'], cols['Saon'], cols['Street'], cols['Postcode'])
             price_rec['Price'] = int(cols['Price'])
             price_rec['Date'] = cols['PriceDate'][0:10]  # yyyy-mm-dd only
             price_rec['Locality'] = cols['Locality']
@@ -74,11 +80,12 @@ def format_for_msg(rec):
 
         else:
             price_rec = None
-    
+
     return price_rec
 
+# ---------------------------------------------------------------------------------------#
 
-#---------------------------------------------------------------------------------------#
+
 def get_outcode(rec):
     """for CSV files, decode a single line that may be quote delimited with embedded 
        commas. Return only the Outcode (first part of the postcode)
@@ -86,10 +93,10 @@ def get_outcode(rec):
             rec: a CSV string representing a data record of columns
             Return: the tuple of Outcode and the Price of the record
     """
-    in_cols = ['RowKey','Price','PriceDate','Postcode','PropertyType',\
-               'NewBuild','Duration','Paon','Saon','Street','Locality',\
-               'Town','District','County','PpdCategory','RecStatus']
-    
+    in_cols = ['RowKey', 'Price', 'PriceDate', 'Postcode', 'PropertyType',
+               'NewBuild', 'Duration', 'Paon', 'Saon', 'Street', 'Locality',
+               'Town', 'District', 'County', 'PpdCategory', 'RecStatus']
+
     decoded_rec = csv.DictReader([rec], in_cols)
     outcode = None
     price = None
@@ -98,5 +105,5 @@ def get_outcode(rec):
         if len(cols['Postcode']) > 0:
             outcode = cols['Postcode'].split(' ')[0].lower()
             price = float(cols['Price'])
-    
+
     return outcode, price
