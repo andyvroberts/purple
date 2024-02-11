7e99490e2ca99f12568d708266b38d7c4b3bad25
Andy Roberts <avroberts@outlook.com>
2024-02-11 12:11:44 +0000

    setup for v2

--- README.md
+++ README.md
@@ -1,21 +1,42 @@
-# Ingest Historical Land Registry Property Prices
-Ingest the UK land registry update file of sold residential property prices.  Store them in table storage.  Create the application using Python.
-
-![Build Status](https://github.com/avroberts-azure/purple/actions/workflows/test-deploy.yml/badge.svg)  
-
-
-## Application Design
-[Read this file for the requirements, design choices and changes throughout the implementaton process](docs/design.md)  
-
-## Useful Information
-### 1. Dotnet
-Setup the dotnet and Azure SKD's before you do anything else.  
-[Read this file for Azure setup and configuration](docs/azure.md)  
-
-### 2. Python
-[Read this file for Python setup and configuration](docs/python.md)  
-
-### 3. Deploy an Aure Function App
-[Read this file for CLI commands to create resources and the Functions Core Tools Deploy command](docs/funcapp.md) 
+# V2: Create a Query Endpoint for Accessing UK Property Prices by Postcode
+Requirement:  
+Create sets of UK Property Prices, grouped by Postcode, to be queried by a sample web application.
+  
+## Azure
+There are three components:  
+1. Create the Postode list as configuration
+    - Run a Python function locally to Create a unique list of postcodes in the PPD file
+    - Save the postcode list in a configuration table in Azure Table Storage
+2. Scan the PPD file for each postcode in the configuration
+    - Read the configuration for the next postcode to process
+    - Run a Python function locally to search the full PPD file and group the property prices for each address into a single dataset
+    - Store the dataset in an Azure Storage Queue for later processing
+3. Load the Postcode dataset into a Database
+    - Create an Azure Storage Table to contain property prices.  Partition by Postcode and Address
+    - Create an Azure Function to read Queue messages and insert the prices into the Table
 
 <br>
+
+
+
+```mermaid
+%%{init: {'theme':'dark'}}%%
+flowchart TD;
+    subgraph Scanner[Scan PPD Locally]
+        direction LR
+        Scan[Scan PPD] --> Extract[Extract Unique Postcodes] --> Store[Save in Configuration]
+    end;
+    subgraph Reader[Read PPD Locally]
+        direction LR
+        Next[Get Next Postcode] --> Search[Read PPD] --> Group[Group by Postcode] --> Queue[Add to Queue]
+    end;
+    subgraph Loader[Load Prices using Azure Functions]
+        direction LR
+        Dequeue[Pop Message] --> Insert[Insert into Table] --> Config[Mark Config Done] 
+    end;
+    Scanner --> Reader --> Loader
+```
+
+## Python Setup
+
+
---
+++ v1/src/prices_loader/__init__.py
@@ -0,0 +1,93 @@
+import logging
+import time
+import ast
+
+import azure.functions as func
+
+from utils import http_reader
+from utils import storage_table
+from utils import common
+
+# ---------------------------------------------------------------------------------------#
+
+
+def lookup_price_and_prep_record(price_table, price_record):
+    # set the partition and rowkeys for searching the table entities.
+    new_entity = storage_table.create_entity_from_price_record(price_record)
+
+    # do the storage table related work
+    return storage_table.lookup_price_entity(price_table, new_entity)
+
+# ---------------------------------------------------------------------------------------#
+
+
+def store_prices(outcode):
+    # set up the queue client
+    price_table = storage_table.get_table_client(common.price_table_name())
+
+    # identify the next data file url based on configuration settings
+    url = common.get_data_url()
+    logging.info(f'price file = {url}.')
+
+    # set up price table batch collection
+    operation_batch = []
+    batch_count = 0
+    total_count = 0
+    record_count = 0
+
+    for stream_rec in http_reader.stream_file_for_outcode(url, outcode):
+        record_count += 1
+        price_rec = lookup_price_and_prep_record(price_table, stream_rec)
+
+        if price_rec is not None:
+            operation_batch.append(price_rec)
+            batch_count += 1
+
+            if batch_count == 100:
+                storage_table.upsert_replace_batch(
+                    price_table, operation_batch)
+                operation_batch = []
+                total_count += batch_count
+                batch_count = 0
+
+    # insert final batch that did not reach the batch limit of 100
+    if len(operation_batch) > 0:
+        storage_table.upsert_replace_batch(price_table, operation_batch)
+        total_count += batch_count
+
+    logging.info(
+        f'PRICE_LOADER for {outcode}. Price File records used = {record_count}'
+    )
+    return total_count
+
+# ---------------------------------------------------------------------------------------#
+
+
+def main(msg: func.QueueMessage) -> None:
+    start_exec = time.time()
+    # suppress or show messages from Azure loggers.
+    logging.getLogger("azure.core.pipeline").setLevel(logging.ERROR)
+    logging.getLogger("urllib3.connectionpool").setLevel(logging.ERROR)
+    logging.getLogger('azure.storage.queue').setLevel(logging.ERROR)
+
+    outcode = msg.get_body().decode('utf-8').lower()
+    id = msg.id
+    pop = msg.pop_receipt
+    logging.info(f'PRICE_LOADER. Processing Outcode: {outcode}')
+
+    # read the price records and process them
+    stored = store_prices(outcode)
+    logging.info(
+        f'PRICE_LOADER for {outcode}. Table records created or modified = {stored}'
+    )
+
+    # delete the message from the load-outcodes queue as this workload has completed.
+    # the SDK automatically deletes triggering messages which complete without error.
+
+    end_exec = time.time()
+    duration = end_exec - start_exec
+    hours, hrem = divmod(duration, 3600)
+    mins, secs = divmod(hrem, 60)
+    logging.info(
+        f'PRICE_LOADER for {outcode}. Execution Duration: {hours}hrs, {mins}mins, {round(secs,0)}secs'
+    )
