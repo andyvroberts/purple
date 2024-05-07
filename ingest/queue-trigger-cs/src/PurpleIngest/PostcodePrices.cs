ef94ee0ea0274aec47d932137ad42bccb526ef70
Andy.R <avroberts@outlook.com>
2024-05-07 19:06:43 +0100

    remove tableoutput binding and create client manually

--- ingest/queue-trigger-cs/src/PurpleIngest/PostcodePrices.cs
+++ ingest/queue-trigger-cs/src/PurpleIngest/PostcodePrices.cs
@@ -1,14 +1,18 @@
+using System;
+using Azure.Data.Tables;
 using Azure.Storage.Queues.Models;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
+
 
 namespace PurpleIngest
 {
     public class PostcodePrices
     {
+        private static TableClient? tabClient;
+
         [Function(nameof(PostcodePrices))]
-        [TableOutput("prices")]
-        public static PriceData Run(
+        public static void Run(
             [QueueTrigger("prices")] QueueMessage msg,
             FunctionContext context)
         {
@@ -21,18 +25,30 @@ namespace PurpleIngest
                 var postcode = msgParts[0];
                 var addressString = msgParts[1];
 
-                _logger.LogInformation($"Postcode: {postcode}");
-
                 tabRow.PartitionKey = postcode.Split(' ')[0];
                 tabRow.RowKey = postcode;
                 tabRow.Addresses = addressString;
+
+                var tabClient = GetTableClient("prices"); 
+                tabClient.UpsertEntity(tabRow);
+
+                _logger.LogInformation($"Postcode {postcode}: table entity updated or inserted.");
             }
             else
             {
                 _logger.LogError($"Queue Message Body is empty.");
             }
-            
-            return tabRow;
+        }
+
+        private static TableClient GetTableClient(string tabName)
+        {
+            if (tabClient == null)
+            {
+                var connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+                tabClient = new TableClient(connectionString, tabName);
+            }
+            return tabClient;
+
         }
     }
 }
